cdb0d868f8862de074237e91048af4fcbcd47415
Sumanta Chakraborty <Sumanta.Chakraborty@cerner.com>
2021-06-28 22:17:08 +0530

    Add Basic Project Structure
    Add Basic Project Structure

---
+++ src/main/java/com/weather/lookup/app/WeatherLookupApplication.java
@@ -0,0 +1,15 @@
+package com.weather.lookup.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class WeatherLookupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherLookupApplication.class, args);
+    }
+}
